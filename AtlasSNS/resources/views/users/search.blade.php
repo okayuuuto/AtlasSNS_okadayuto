29dffa2f1fae698199b122deb9a41308e8df948f
岡田祐人 <>
2023-06-05 18:11:55 +0900

    検索ワードの入力フォームを設置

--- AtlasSNS/resources/views/users/search.blade.php
+++ AtlasSNS/resources/views/users/search.blade.php
@@ -2,6 +2,15 @@
 
 @section('content')
 
+{!! Form::open(['url' => '/search']) !!}
 
+<div class="search_form">
+  {!! Form::textarea('search', null, ['required', 'class' => 'search_form_control', 'placeholder' => 'ユーザー名', 'rows' => 1]) !!}
+
+  <input type="image" name="submit" img class="search_icon" src="{{asset('images/search.png')}}" alt="送信">
+  </input>
+</div>
+
+{!! Form::close() !!}
 
 @endsection
