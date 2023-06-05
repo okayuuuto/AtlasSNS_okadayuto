1bbe8ab0b8d7adcab7d34adaa3f8ed48d634d809
岡田祐人 <>
2023-06-06 03:01:41 +0900

    ユーザー検索と検索ワードの表示

--- AtlasSNS/app/Http/Controllers/UsersController.php
+++ AtlasSNS/app/Http/Controllers/UsersController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\User;
+use Auth;
 
 class UsersController extends Controller
 {
@@ -10,7 +12,27 @@ class UsersController extends Controller
     public function profile(){
         return view('users.profile');
     }
-    public function search(){
-        return view('users.search');
+
+    //ユーザー一覧
+    public function index() {
+
+        if ($request->has('search')) {
+            return $this->search($request);
+        }
+
+        $users = User::where('id', '!=', Auth::id())->get();
+        return view('users.search',['users'=> $users]);
+    }
+
+    //ユーザー検索
+    public function search(Request $request){
+        $keyword = $request->input('search');
+        if(!empty($keyword)){
+            $users = User::where('username','like','%'.$keyword.'%')->orderBy('created_at', 'desc')->get();
+        }else{
+            $users = User::where('id', '!=', Auth::id())->orderBy('created_at', 'desc')->get();
+        }
+
+        return view('users.search', ['users' => $users, 'keyword' => $keyword]);
     }
 }
--- AtlasSNS/resources/views/users/search.blade.php
+++ AtlasSNS/resources/views/users/search.blade.php
@@ -2,15 +2,24 @@
 
 @section('content')
 
-{!! Form::open(['url' => '/search']) !!}
+{!! Form::open(['url' => '/search', 'method' => 'GET']) !!}
 
 <div class="search_form">
-  {!! Form::textarea('search', null, ['required', 'class' => 'search_form_control', 'placeholder' => 'ユーザー名', 'rows' => 1]) !!}
+  {!! Form::text('search', null, ['class' => 'search_form_control', 'placeholder' => 'ユーザー名']) !!}
 
   <input type="image" name="submit" img class="search_icon" src="{{asset('images/search.png')}}" alt="送信">
   </input>
+  <p>検索ワード： {{ $keyword }}</p>
 </div>
 
 {!! Form::close() !!}
+
+@foreach ($users as $user)
+<ul>
+  <li></li>
+  <li>{{ $user->username }}</li>
+  <li></li>
+</ul>
+@endforeach
 
 @endsection
